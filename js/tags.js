5855f4742c989a2d20184f747fac694f96e5ff6b
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-07-23 13:08:54 +0100

    Added Helper functions

--- js/tags.js
+++ js/tags.js
@@ -1,31 +1,33 @@
-(function(){
-    
-}) ()
+// Helper function to add dropdown options
+const addDropdownOption = (category, name) => {
+	const dropdown = document.querySelector(`#${category}Dropdown .dropdown-body`);
+	dropdown.innerHTML += `<a href="#" onclick='categoryClick(this, "${category}")'>${name}</a>`;
+};
 
+const addNewCategory = (nameCat) => {
+	const categoriesContainer = document.querySelector('#categoriesContainer');
+	categoriesContainer.insertAdjacentHTML(
+		'beforeend',
+		`<li class="categoryRecipes">
+          <span class="nameCat">${nameCat}</span>
+          <span onclick="removeCategory(this)" class="btnRemove bold">
+            <i class="fas fa-times"></i>
+          </span>
+      </li>`
+	);
+	functionSearch();
+};
 
-function addNewCategory(nameCat){
-    document.querySelector('#categoriesContainer').insertAdjacentHTML('beforeend',
-    `<li class="categoryRecipes">
-        <span class="nameCat">${nameCat}</span>
-        <span onclick="removeCategory(this)" class="btnRemove bold"> <i class="fas fa-times"></i>  </span>
-    </li>`
-    )
-    functionSearch()
-}
-//Remove Category
-function removeCategory(e){
-    let tagIndex = tags.findIndex(x => x.name === e.parentElement.querySelector('.nameCat').innerText)
-    if(tags[tagIndex].category === 'ingredient') {
-        ingredientsDropdown.innerHTML += `<a href="#" onclick='categoryClick(this, "ingredient")'>${tags[tagIndex].name}</a>`
-    }
-    if(tags[tagIndex].category === "appliance") {
-        deviceDropdown.innerHTML += `<a href="#" onclick='categoryClick(this, "appliance")'>${tags[tagIndex].name}</a>`
-    }
-    if(tags[tagIndex].category === 'ustensils') {
-        ustensilsDropdown.innerHTML += `<a href="#" onclick='categoryClick(this, "ustensils")'>${tags[tagIndex].name}</a>`
-    }
-    tags.splice(tagIndex, 1)
-    
-    e.parentElement.remove()
-    functionSearch()
-}
+const removeCategory = (e) => {
+	const tagName = e.parentElement.querySelector('.nameCat').innerText;
+	const tagIndex = tags.findIndex((x) => x.name === tagName);
+
+	if (tagIndex !== -1) {
+		const tag = tags[tagIndex];
+		addDropdownOption(tag.category, tag.name);
+		tags.splice(tagIndex, 1);
+	}
+
+	e.parentElement.remove();
+	functionSearch();
+};
